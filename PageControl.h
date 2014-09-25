d22c4854dd112049a8dae30cb06f536a4f858e9f
Kireto <kireto@outlook.com>
2014-09-25 11:29:19 +0200

    Create PageControl.h

---
+++ PageControl.h
@@ -0,0 +1,12 @@
+//
+//  PageControl.h
+//
+//  Created by Kireto.
+//  Copyright (c) 2014 Kireto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PageControl : UIPageControl
+
+@end
